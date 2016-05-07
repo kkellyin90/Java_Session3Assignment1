c4ad7aa5e0ccb77a153c131ecc07da4acb75fc6a
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-07 19:19:53 +0530

    Added files via upload

---
+++ squareCubeNo.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+public class squareCubeNo {
+
+	public static void main(String[] args) {
+		
+		double square = 64;
+		double cube = 125;
+		
+		System.out.println("The square root of "+square + " is " + Math.sqrt(square));
+		System.out.println("The cube root of "+cube + " is " + Math.cbrt(cube));
+	
+
+	}
+
+}
+
+		
+		 
+		
+	
